e6acdd75b7960d64ba07ff2a171b93dd7232e1de
Alex2055 <alexfromnashville@gmail.com>
2020-09-13 12:17:28 -0500

    spinner function updated

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,24 +1,7 @@
 
 const signupFormHandler = async function(event) {
-  $( document ).on( "click", ".show-page-loading-msg", function() {
-    var $this = $( this ),
-        theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
-        msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,
-        textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
-        textonly = !!$this.jqmData( "textonly" );
-        html = $this.jqmData( "html" ) || "";
-    $.mobile.loading( "show", {
-            text: msgText,
-            textVisible: textVisible,
-            theme: theme,
-            textonly: textonly,
-            html: html
-    });
-})
-.on( "click", ".hide-page-loading-msg", function() {
-    $.mobile.loading( "hide" );
-});
      event.preventDefault();
+     showSpinner($(this));
      const email = document.querySelector("#email");
      const password = document.querySelector("#password");
     fetch("/api/users", {
@@ -33,7 +16,10 @@ const signupFormHandler = async function(event) {
         .then(function() {
           document.location.replace("/home");
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+          $.mobile.loading( "hide" );
+          console.log(err)
+        });
     };
 
     document
