182aa07fafaa9a0af01eb8976398e6dfb9d0f85a
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-09-14 17:06:59 -0500

    added add store form

---
+++ public/javascript/addStore.js
@@ -0,0 +1,33 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const store_name = document.querySelector('input[name="store-name"]').value;
+    const city = document.querySelector('input[name="city"]').value;
+    const state = document.querySelector('input[name="state"]').value;
+    const zip = document.querySelector('input[name="zip"]').value;
+
+    const response = await fetch(`/api/stores`, {
+        method: 'POST',
+        body: JSON.stringify({
+          store_name,
+          city,
+          state,
+          zip,
+          user_id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+          console.log("Store added");
+          console.log(response);
+        // document.location.replace('/search-view');
+      } else {
+          console.log("Store not added");
+        alert(response.statusText);
+      }
+    }
+    
+    document.querySelector('.add-store-form').addEventListener('submit', newFormHandler);  
